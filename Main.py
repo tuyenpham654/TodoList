67bf2f62d3296d72fa3ae572cef6281edaa4ddb1
tuyenpham654 <tuyenpham654@gmail.com>
2024-05-23 13:26:28 +0700

    Login
    basic

--- Main.py
+++ Main.py
@@ -1,29 +1,33 @@
 from tkinter import *
-import data
-
-data.conn
-
+import data as d
 
 root = Tk()
 root1 = Tk()
 root1.withdraw()
-
+root.title("Login")
+root1.title("To Do List")
 
 
 
 def login():
 
-    cus=data.conn.cursor()
-    Label(root,text=txtUserName.get()).grid(row=3,column=1)
-    Label(root,text=txtPW.get()).grid(row=3,column=2)
+    cus=d.conn.cursor()
+    
     try:
         sql = "select 1 from Account where UserName = ? and [Password]=?"
-        cus.execute(sql,(txtUserName,txtPW))
+        cus.execute(sql,(txtUserName.get(),txtPW.get()))
         resultlg = cus.fetchall()
     except:
         resultlg = ()
 
     a=len(resultlg)
+    if a == 1:
+        if root1.state() == "withdrawn":
+            root.withdraw()
+            root1.deiconify()
+        else:
+            root1.withdraw()
+            root.deiconify()
     
 
 
@@ -32,7 +36,8 @@ def login():
 txtUserName = StringVar()
 
 txtPW = StringVar()
-root.minsize(height=500, width=500)
+root.minsize(height=200, width=300)
+root1.minsize(height=600, width=600)
 
 Label(root, text="Tên đăng nhập").grid(row=0,column=0)
 Entry(root,textvariable=txtUserName).grid(row=0,column=1)
@@ -42,8 +47,8 @@ Label(root, text="Tên đăng nhập").grid(row=1,column=0)
 Entry(root,textvariable=txtPW).grid(row=1,column=1)
 
 
-Button(root, text="Đăng Nhập",command=login).grid(row=2,column=1)
-Button(root, text="Thoát",command=root.quit).grid(row=2,column=2)
+Button(root, text="Đăng Nhập",command=login).grid(row=2,column=0)
+Button(root, text="Thoát",command=root.quit).grid(row=2,column=1)
 
 
 
--- test.py
+++ test.py
@@ -1,35 +1,30 @@
-import pyodbc
+import pyodbc,data as d
 import tkinter as tk
 
-conn = pyodbc.connect(
-    "Driver={SQL Server};"
-    "Server=STEVE;"
-    "Database=QLNhaTro;"
-    "Trusted_Connection=yes;"
-)
 
 
-# Tạo hai cửa sổ
-root1 = tk.Tk()
-root2 = tk.Tk()
 
-# Ẩn cửa sổ root2 ban đầu
-root2.withdraw()
-# Hàm callback khi bấm vào nút
-def toggle_windows():
-    if root2.state() == "withdrawn":
-        root1.withdraw()
-        root2.deiconify()
-    else:
-        root2.withdraw()
-        root1.deiconify()
+# # Tạo hai cửa sổ
+# root1 = tk.Tk()
+# root2 = tk.Tk()
 
-# Tạo nút bấm trong root1
-button = tk.Button(root1, text="Chuyển đổi cửa sổ", command=toggle_windows)
-button.pack()
-# Hiển thị root1
-root1.mainloop()
-cus = conn.cursor()
+# # Ẩn cửa sổ root2 ban đầu
+# root2.withdraw()
+# # Hàm callback khi bấm vào nút
+# def toggle_windows():
+#     if root2.state() == "withdrawn":
+#         root1.withdraw()
+#         root2.deiconify()
+#     else:
+#         root2.withdraw()
+#         root1.deiconify()
+
+# # Tạo nút bấm trong root1
+# button = tk.Button(root1, text="Chuyển đổi cửa sổ", command=toggle_windows)
+# button.pack()
+# # Hiển thị root1
+# root1.mainloop()
+cus = d.conn.cursor()
 
 us = input("Nhập tài khoản: ")
 pw = input("Nhập mật khẩu: ")
